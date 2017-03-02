5a4e9b7138b856e77633196508aa9aab30bc4279
xql <985045893@qq.com>
2017-03-02 12:47:08 +0800

    1

---
+++ exe in panel/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.appContainer1 = new SmileWei.EmbeddedApp.AppContainer(this.components);
+            this.SuspendLayout();
+            // 
+            // appContainer1
+            // 
+            this.appContainer1.AppFilename = "";
+            this.appContainer1.AppProcess = null;
+            this.appContainer1.BackColor = System.Drawing.Color.Transparent;
+            this.appContainer1.Location = new System.Drawing.Point(337, 222);
+            this.appContainer1.Name = "appContainer1";
+            this.appContainer1.Size = new System.Drawing.Size(524, 345);
+            this.appContainer1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1110, 622);
+            this.Controls.Add(this.appContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.load1);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private SmileWei.EmbeddedApp.AppContainer appContainer1;
+
+
+
+
+
+
+
+
+    }
+}
+
---
+++ exe in panel/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //MessageBox.Show(this.appContainer1.Handle.ToInt32().ToString());
+            //写入txt
+            StreamWriter sw = new StreamWriter("D:\\1.txt");
+            string w = this.appContainer1.Handle.ToInt32().ToString();
+            sw.Write(w);
+            sw.Close();
+            Process process;
+            this.appContainer1.AppFilename =
+                "C:\\Users\\Frank\\Desktop\\test8.exe";
+            this.appContainer1.Start();
+//            process = new Process();
+//            process.StartInfo.FileName = Application.StartupPath+"test5.exe";
+//            process.Start();
+
+                
+        }
+
+        private void load1(object sender, EventArgs e)
+        {
+            this.BackColor = Color.White;
+            this.TransparencyKey = Color.White;
+        }
+
+        
+    }
+}
